d1533ab19799f9c954d0c7987e91d94b4b15c114
Tomerd-afk <67992389+Tomerd-afk@users.noreply.github.com>
2022-01-31 19:01:51 -0500

    Added Classes AiV2 and Factor
    Began Implementation of Tier 1 items to create abstract enemy classes!

---
+++ Assets/Scripts/AI/Aiv2.cs
@@ -0,0 +1,201 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Aiv2 : SelfDespawn
+{
+
+
+    //Variables that can be set when the new abstract Enemy is contructed 
+    public string tag;
+    public int startingHP;
+    public int speed;
+    public int attackRange;
+    public int score;
+    public bool alive;
+
+    //complex 
+    public Rigidbody rb;
+    public Gun myGun;
+    public CyborgAnimationStateController animationStateController;
+    public GameObject target;
+    public Health hp;
+
+
+    public override void Init()
+    {
+        SetStats();
+        #region Error Checking
+        if (animationStateController == null)
+        {
+            Debug.LogError("This object needs a CyborgAnimationStateController component");
+        }
+        if (rb == null)
+        {
+            Debug.LogError("This object needs a RigidBody component");
+        }
+        if (myGun == null)
+        {
+            Debug.LogError("This object needs a GUN component");
+        }
+        if (hp == null)
+        {
+            Debug.LogError("This object needs a Health component");
+        }
+        if (target == null)
+        {
+            Debug.LogError("This object needs a Target to Pursue");
+        }
+        #endregion
+        throw new System.NotImplementedException();
+    }
+
+    public override void Update()
+    {
+        base.Update();//Checks if AI is out of bounds 
+        
+
+        animationStateController.SetSpeed(rb.velocity.magnitude);
+        if (hp.HitPoints <= 0) //this signifies that the enemy Died and wasn't merely Despawned 
+        {
+            myGun.StopAllCoroutines();
+            animationStateController.StopAllCoroutines();
+            alive = false;
+            this.gameObject.SetActive(false);
+        }
+        else
+        {
+            Move(target.transform.position);
+        }
+    }
+
+    public abstract void Move(Vector3 t);
+
+    public void applyForce(Vector3 force)
+    {
+        rb.AddForce(force);
+    }
+
+    public abstract void Attack();
+    public abstract void SetStats();
+
+    #region Getters&Setters  
+    public bool isAlive()
+    {
+        return alive;
+    }
+    public float getScore()
+    {
+        return score;
+    }
+    #endregion
+}
+
+
+public class gruntAi : Aiv2
+{
+    public void Awake()
+    {
+        alive = true;
+        hp = GetComponentInChildren<Health>();
+        hp.Init(startingHP);
+    }
+    public override void Attack()
+    {
+        if (myGun.CanShootAgain())
+        {
+            this.myGun.Shoot(target.transform.position);
+            //TODO: the AI currently doesn't stand still when firing and I'm not sure if they should?
+            animationStateController.Shoot();
+        }
+        throw new System.NotImplementedException();
+    }
+
+    public override void Move(Vector3 target)
+    {
+        Vector3 desiredVec = target - transform.position; //this logic creates the vector between where the entity is and where it wants to be 
+        float dMag = desiredVec.magnitude; //this creates a magnitude of the desired vector. This is the distance between the points 
+        dMag -= attackRange; // dmag is the distance between the two objects, by subtracking this, I make it so the object doesn't desire to move as far.  
+        desiredVec.Normalize(); // one the distance is measured this vector can now be used to actually generate movement,
+                                // but that movement has to be constant or at least adaptable, which is what the next part does  
+        transform.LookAt(target);
+
+        //Currently Walking twoards the target 
+
+        if (dMag < speed)
+        {         
+            desiredVec *= dMag; //Slowing down walking speed as AI approaches Target 
+            if (dMag < attackRange)
+            {
+                Attack();
+            }
+        }
+        else
+        {
+            desiredVec *= speed;
+        }
+
+        Vector3 steer = desiredVec - rb.velocity;
+        applyForce(steer);
+
+
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetStats()
+    {
+        startingHP = 60;
+        speed = 10;
+        attackRange = 10;
+        score = 69;
+        throw new System.NotImplementedException();
+    }
+
+    public void loadout(GameObject player, Gun gun)
+    {
+        target = player;
+        myGun = gun;
+        throw new NotImplementedException();
+    }
+
+
+}
+
+
+public class bikerAi : Aiv2
+{
+
+    public void Awake()
+    {
+        alive = true; 
+        hp = GetComponentInChildren<Health>();
+        hp.Init(startingHP);
+    }
+
+    public override void Attack()
+    {
+        throw new System.NotImplementedException();
+    }//TODO
+
+    public override void Move(Vector3 target)
+    {
+        //Vroom Vroom Drive me Bike 
+
+        throw new System.NotImplementedException();
+
+
+    }//TODO
+
+    public override void SetStats()
+    {
+        startingHP = 20;
+        speed = 10;
+        attackRange = 10;
+        score = 69;
+
+
+        throw new System.NotImplementedException();
+    }//TODO
+
+}
---
+++ Assets/Scripts/AI/Factory.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Factory : MonoBehaviour
+{
+    #region EnemyAI lists
+
+    public List<gruntAi> Riflemen;
+    public gruntAi grunt;
+    public float gruntPoolSize;
+    public List<bikerAi> Bikers;
+    public bikerAi biker;
+    public float bikerPoolSize;
+
+    public List<Gun> guns;
+
+
+    #endregion
+
+    public GameObject player;
+    public int spawnDistance = 30;
+
+    #region Getters&Setters
+    public Vector3 generateSpawnVector()
+    {
+        Vector3 spawnVector = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z + spawnDistance);
+        Quaternion ranRot = Quaternion.Euler(0, Random.Range(0, 359), 0);
+
+        spawnVector = ranRot * spawnVector;
+
+        return spawnVector;
+    }
+    public Gun getGun()
+    {
+        return guns[0];
+    }//TODO Hard code in list of all guns 
+    #endregion
+
+    
+    public void op_KILLME(SelfDespawn entity) // THis is the method that sets the entity to Deactive and bascially is uesd to kill the entitiy 
+    {
+        entity.gameObject.SetActive(false);
+        //TODO: Add Logic here to make sure Entity either remains in the pool or becomes a new entity
+    }
+
+    void Start()
+    {
+
+        Riflemen = new List<gruntAi>();
+        for(int i=0; i < gruntPoolSize; i++)
+        {
+            gruntAi newEnemy = Instantiate(grunt, generateSpawnVector(), Quaternion.identity);
+            newEnemy.Init();
+            newEnemy.loadout(player, getGun());
+            newEnemy.Despawn += op_KILLME; //this line adds the despawn event to this entity 
+            newEnemy.gameObject.SetActive(true);
+
+            Riflemen.Add(newEnemy);
+        }
+
+        //TODO add Stuff here for BIKER 
+    }
+
+    public void Respawn(Aiv2 dedDude)
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
